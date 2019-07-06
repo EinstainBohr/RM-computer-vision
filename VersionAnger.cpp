e33bea193a70d6c601ea2b3e78f5de48751abaa1
Einstain <37773638+EinstainBohr@users.noreply.github.com>
2019-07-06 22:22:15 +0800

    Update VersionAnger.cpp

--- VersionAnger.cpp
+++ VersionAnger.cpp
@@ -143,15 +143,15 @@ int main()
            vector<cv::Point3f> objectPoints;                //小装甲板空间坐标
            objectPoints.push_back(cv::Point3f(0, 0, 0));
            objectPoints.push_back(cv::Point3f(13.5, 0, 0));
-           objectPoints.push_back(cv::Point3f(13.5, 5.4, 0));
-           objectPoints.push_back(cv::Point3f(0, 5.4, 0));
+           objectPoints.push_back(cv::Point3f(13.5, 5.6, 0));
+           objectPoints.push_back(cv::Point3f(0, 5.6, 0));
            
 
            vector<cv::Point3f> objectPoints2;                //大装甲板空间坐标
            objectPoints2.push_back(cv::Point3f(0, 0, 0));
            objectPoints2.push_back(cv::Point3f(23, 0, 0));
-           objectPoints2.push_back(cv::Point3f(23, 5.4, 0));
-           objectPoints2.push_back(cv::Point3f(0, 5.4, 0));
+           objectPoints2.push_back(cv::Point3f(23, 5.6, 0));
+           objectPoints2.push_back(cv::Point3f(0, 5.6, 0));
 
 
            vector<cv::Point2f> imagePoints;                 //装甲板像素坐标
